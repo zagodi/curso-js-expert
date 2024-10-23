1e0fe3304d2956b761d2c17fd98edabca8422281
Zagodi <diego.s.zago1995@gmail.com>
2024-10-23 19:52:42 -0300

    Add files for aula 05 part 03

---
+++ module01/aula05-tdd-project-pt03/src/entities/transaction.js
@@ -0,0 +1,10 @@
+class Transaction {
+  constructor({ customer, car, amount, dueDate }) {
+    customer = this.customer;
+    car = this.car;
+    amount = this.amount;
+    dueDate = this.dueDate;
+  }
+}
+
+module.exports = Transaction;
